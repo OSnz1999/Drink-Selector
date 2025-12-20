2b68531093b91a89f14d7aafec69fc25dcd667e2
OSnz1999 <smith.oliver.099@gmail.com>
2025-12-20 14:58:54 +1300

    UI Polish: Add global admin link

--- app/layout.tsx
+++ app/layout.tsx
@@ -17,6 +17,12 @@ export default function RootLayout({
       <body className={clsx("antialiased")} suppressHydrationWarning>
         <main className="container">
           {children}
+
+          <div className="fixed bottom-2 right-2 z-50 opacity-0 hover:opacity-100 transition-opacity">
+            <a href="/admin" className="p-2 bg-slate-800/50 rounded-full text-xs text-slate-500 hover:text-white hover:bg-slate-700 block">
+              Admin
+            </a>
+          </div>
         </main>
       </body>
     </html>
